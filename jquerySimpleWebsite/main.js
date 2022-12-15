ed066a87732f6fd0838f155a9fe9a1e6556436b3
MZhann <90520809+MZhann@users.noreply.github.com>
2022-12-15 16:08:44 +0600

    JquerySimple app with zoom of photos

---
+++ jquerySimpleWebsite/main.js
@@ -0,0 +1,56 @@
+conole.log('hello');
+$(document).ready(function(){
+    //show popup
+    $("#show-popup-btn").click(function(){
+        $("#modal").show(); 
+    })
+    //close popup
+    $("#close-btn").click(function(){
+        $("#modal").hide();
+    })
+    //2
+    //show popup
+    $("#show-popup-btn1").click(function(){
+        $("#modal1").show(); 
+    })
+    //close popup
+    $("#close-btn1").click(function(){
+        $("#modal1").hide();
+    })
+    //3
+    //show popup
+    $("#show-popup-btn2").click(function(){
+        $("#modal2").show(); 
+    })
+    //close popup
+    $("#close-btn2").click(function(){
+        $("#modal2").hide();
+    })
+    //4
+    //show popup
+    $("#show-popup-btn3").click(function(){
+        $("#modal3").show(); 
+    })
+    //close popup
+    $("#close-btn3").click(function(){
+        $("#modal3").hide();
+    })
+    //5
+    //show popup
+    $("#show-popup-btn4").click(function(){
+        $("#modal4").show(); 
+    })
+    //close popup
+    $("#close-btn4").click(function(){
+        $("#modal4").hide();
+    })
+    //6
+    //show popup
+    $("#show-popup-btn5").click(function(){
+        $("#modal5").show(); 
+    })
+    //close popup
+    $("#close-btn5").click(function(){
+        $("#modal5").hide();
+    })
+})
